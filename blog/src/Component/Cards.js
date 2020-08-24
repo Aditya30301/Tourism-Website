cf3cd8b9b9d6b2fdad2b7c548f2037b8fedb082d
Aditya Raj SIngh <69067246+Aditya30301@users.noreply.github.com>
2020-08-24 20:10:56 +0530

    Added Database and And Dynamic rendering of Card Component

--- blog/src/Component/Cards.js
+++ blog/src/Component/Cards.js
@@ -10,7 +10,7 @@ import Collapse from "@material-ui/core/Collapse";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
+
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme) => ({
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function RecipeReviewCard() {
+export default function RecipeReviewCard(props) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
@@ -46,27 +46,14 @@ export default function RecipeReviewCard() {
 
   return (
     <Card className={classes.root}>
-      <CardHeader
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        className={classes.media}
-        image="https://haptik-website-images.haptik.ai/assets/img/IVA-whitepaper.png"
-        title="Paella dish"
-      />
+      <CardHeader title={props.name} subheader={props.duration} />
+      <CardMedia className={classes.media} image={props.imageLink} />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {props.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
@@ -80,33 +67,15 @@ export default function RecipeReviewCard() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
+          <Typography paragraph>Trip Details:</Typography>
+          <Typography paragraph>{props.fullDescription1}</Typography>
+          <Typography paragraph>{props.fullDescription2}</Typography>
+          <Typography paragraph>{props.fullDescription3}</Typography>
+          <Typography paragraph>{props.fullDescription4}</Typography>
+          <Typography paragraph>{props.fullDescription5}</Typography>
+          <Typography paragraph>{props.fullDescription6}</Typography>
+          <Typography paragraph>{props.fullDescription7}</Typography>
+          <Typography paragraph>{props.fullDescription8}</Typography>
         </CardContent>
       </Collapse>
     </Card>
--- blog/src/Menu/Service.js
+++ blog/src/Menu/Service.js
@@ -1,6 +1,27 @@
 import React from "react";
 import Cards from "../Component/Cards";
+import datas from "./CardData";
 const Service = () => {
+  const cardObject = datas.map(function (data) {
+    return (
+      <Cards
+        key={data.id}
+        name={data.name}
+        duration={data.duration}
+        description={data.description}
+        fullDescription1={data.fullDescription1}
+        fullDescription2={data.fullDescription2}
+        fullDescription3={data.fullDescription3}
+        fullDescription4={data.fullDescription4}
+        fullDescription5={data.fullDescription5}
+        fullDescription6={data.fullDescription6}
+        fullDescription7={data.fullDescription7}
+        fullDescription8={data.fullDescription8}
+        imageLink={data.imageLink}
+      />
+    );
+  });
+
   return (
     <>
       <div className="container-fluid nav_bg" container>
@@ -8,8 +29,8 @@ const Service = () => {
           <div className="col-10 mx-auto align-items-center">
             <h1>
               <center>Our Services</center>
+              {cardObject}
             </h1>
-            <Cards />
           </div>
         </div>
       </div>
