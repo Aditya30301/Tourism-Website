6bec1dc06adbf0a1bcbfc05662f95cfec5be2d6e
Aditya Raj SIngh <69067246+Aditya30301@users.noreply.github.com>
2020-08-27 12:20:12 +0530

    New About Page

--- blog/src/Menu/Service.js
+++ blog/src/Menu/Service.js
@@ -1,24 +1,27 @@
 import React from "react";
 import Cards from "../Component/Cards";
 import datas from "./CardData";
+import Container from "@material-ui/core/Container";
 const Service = () => {
   const cardObject = datas.map(function (data) {
     return (
-      <Cards
-        key={data.id}
-        name={data.name}
-        duration={data.duration}
-        description={data.description}
-        fullDescription1={data.fullDescription1}
-        fullDescription2={data.fullDescription2}
-        fullDescription3={data.fullDescription3}
-        fullDescription4={data.fullDescription4}
-        fullDescription5={data.fullDescription5}
-        fullDescription6={data.fullDescription6}
-        fullDescription7={data.fullDescription7}
-        fullDescription8={data.fullDescription8}
-        imageLink={data.imageLink}
-      />
+      <Container>
+        <Cards
+          key={data.id}
+          name={data.name}
+          duration={data.duration}
+          description={data.description}
+          fullDescription1={data.fullDescription1}
+          fullDescription2={data.fullDescription2}
+          fullDescription3={data.fullDescription3}
+          fullDescription4={data.fullDescription4}
+          fullDescription5={data.fullDescription5}
+          fullDescription6={data.fullDescription6}
+          fullDescription7={data.fullDescription7}
+          fullDescription8={data.fullDescription8}
+          imageLink={data.imageLink}
+        />
+      </Container>
     );
   });
 
