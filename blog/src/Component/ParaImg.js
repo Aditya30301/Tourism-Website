ffd80041ed332d430d8fcddd2c43cf360bdffe9c
dineshlal <dineshlal29@gmail.com>
2020-08-29 11:45:01 +0530

    updated videoapp

--- blog/src/Component/ParaImg.js
+++ blog/src/Component/ParaImg.js
@@ -8,7 +8,7 @@ class ParaImg extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mediaPhoto: [myImage],
+      mediaPhoto: [imgone],
     };
   }
 
--- blog/src/Component/VideoApp.js
+++ blog/src/Component/VideoApp.js
@@ -13,6 +13,7 @@ export default class VideoApp extends Component {
               url={[
                 "https://www.youtube.com/watch?v=uFmEwBTQKDA&t=336s",
                 "https://www.youtube.com/watch?v=jNgP6d9HraI",
+                "https://www.youtube.com/watch?v=yque3Zes0eg",
               ]}
               width="100%"
               height="100%"
